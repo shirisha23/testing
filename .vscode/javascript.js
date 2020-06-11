9f921c8ce136b5323d79690178fe5c855010ea63
sneha <sneha.vps888@gmail.com>
2020-06-11 16:05:13 +0530

    first commit

---
+++ .vscode/javascript.js
@@ -0,0 +1,8 @@
+document.write("chamvaran"+" "+"ammu");
+let a=["cham","champion"];
+a.push(2022);
+let b=[1,8,3,2,9,0,8];
+let c=[7,6,5];
+b.push(15);
+document.write(b.sort());
+document.write(a);
